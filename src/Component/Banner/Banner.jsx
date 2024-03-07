0de74aac91ab0a8a5e02ce4486b6e181b1856c33
Nahidahmmed <nahidahmmed411@gmail.com>
2024-03-07 10:32:27 +0600

    banner and navbar section is ready to go

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -6,9 +6,10 @@ import { IoSearch } from "react-icons/io5";
 function Banner() {
   return (
     <div className="relative">
-      <img className="w-full xl:h-[580px] lg:h-[580px] md:h-[530px] h-[700px] object-cover" src={banner} alt="Banner" />
+      <img className="w-full xl:h-[580px] lg:h-[580px] md:h-[530px] h-[800px] object-cover" src={banner} alt="Banner" />
       <div className="absolute text-center xl:top-20 lg:top-20 md:top-20 top-24 left-0 right-0 bottom-0 flex flex-col items-center justify-center p-4">
-        <div className="bg-white rounded-xl mb-2 xl:w-[90%] lg:w-[90%] md:w-[95%] w-full xl:px-14 lg:px-14 md:px-5 px-6 py-14 ">
+        <h1 className="text-5xl font-medium text-white xl:mr-[40%] lg:mr-[40%] md:mr-[40%] mr-[0%] mb-5">Welcome To <span className="text-blue-600">AirBook</span></h1>
+        <div className="bg-white rounded-xl mb-2 xl:max-w-screen-xl lg:w-[90%] md:w-[95%] w-full xl:px-14 lg:px-14 md:px-5 px-6 xl:py-14 lg:py-14 md:py-14 py-5 ">
           <div className="xl:flex lg:flex md:flex xl:justify-between lg:justify-between md:justify-between mb-5">
             <div className="flex">
               <button className="flex items-center text-xs p-3 rounded-lg my-2 mr-2 bg-[#EEEDEB] hover:bg-gray-200"><FaRegCircle className="text-lg mr-2 text-[#B4B4B8]"/> One Way</button>
@@ -20,20 +21,20 @@ function Banner() {
             </div>
           </div>
           <div className="xl:flex lg:flex md:flex">
-            <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 grid-cols-1 gap-5 w-full">
+            <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 w-full">
               {infos.map((info, indx) => (
                 <div key={indx}>
-                  <div className="flex border-[1px] border-gray-200 items-center rounded-lg hover:bg-gray-100">
-                    <h1 className="text-xl xl:m-4 lg:m-4 md:m-2 m-4 border-r-[2px] xl:pr-4 lg:pr-4 md:pr-2 pr-4 font-semibold">{info.heading}</h1>
+                  <div className="flex border-[1px] border-gray-200 items-center rounded-lg hover:bg-gray-100 xl:p-0 lg:p-0 md:p-2">
+                    <h1 className="text-xl xl:m-4 lg:m-4 md:m-2 m-4 border-r-[2px] xl:pr-4 lg:pr-4 md:pr-2 pr-4 font-semibold w-16">{info.heading}</h1>
                     <div>
                       <p className="text-base text-start font-medium">{info.title}</p>
-                      <p className="lg:text-xs md:text-[10px] text-xs text-start">{info.text}</p>
+                      <p className=" text-xs text-start">{info.text}</p>
                     </div>
                   </div>
                 </div>
               ))}
             </div>
-            <button className="ml-5 bg-yellow-500 rounded-xl w-28 hover:bg-yellow-600">
+            <button className="xl:ml-5 lg:ml-5 md:ml-5 ml-0 bg-yellow-500 rounded-xl xl:w-28 lg:w-28 md:w-28 w-full hover:bg-yellow-600 xl:mt-0 lg:mt-0 md:mt-16 mt-5 xl:h-auto lg:h-auto md:h-20 h-16">
               <IoSearch className="mx-auto text-3xl text-white" />
             </button>
           </div>
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -72,7 +72,7 @@ export default function Navbar() {
               <line x1="4" x2="20" y1="18" y2="18" />{" "}
             </svg>
             {dropDownState && (
-              <ul className=" z-10  gap-2  bg-[#393E46]  absolute right-0 top-11 flex w-[200px] flex-col  rounded-lg   text-base ">
+              <ul className=" z-10 gap-2 bg-white shadow-lg absolute -right-3 top-14 flex w-[200px] flex-col rounded-lg text-base ">
                 <li className="cursor-pointer  px-6 py-2  rounded-t-lg hover:bg-sky-600 ">
                   Home
                 </li>
